7692c1f7940e51ba71134b2cd3ff2ff615470c24
michelbui1986 <92622165+michelbui1986@users.noreply.github.com>
2023-06-19 15:16:32 +0200

    19 juin 2023

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/header/Navbar";
 import Newnav from "./components/newnavbar/Newnav";
 import Maincomp from "./components/home/Maincomp";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <Newnav />
       <Maincomp />
+      <Footer />
     </>
   );
 }
