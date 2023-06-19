e72bbbc81e0becb07956424b9435e34264a2f3b1
michelbui1986 <92622165+michelbui1986@users.noreply.github.com>
2023-06-19 10:59:58 +0200

    'first'

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/header/Navbar";
+import Newnav from "./components/newnavbar/Newnav";
+import Maincomp from "./components/home/Maincomp";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Newnav />
+      <Maincomp />
+    </>
+  );
+}
+
+export default App;
